858242788dd3fc2511b16cfff56a963dc4854eca
Ayca Badem <aycabadem12@gmail.com>
2024-01-03 15:25:55 +0000

    added content loader

--- src/ImageComponent.js
+++ src/ImageComponent.js
@@ -1,4 +1,17 @@
 import React, { useState } from "react";
+import ContentLoader from "react-content-loader";
+const MyImageLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={100}
+    height={200}
+    viewBox="0 0 200 200"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="0" rx="10" ry="10" width="200" height="200" />
+  </ContentLoader>
+);
 
 const ImageComponent = ({ src, alt }) => {
   const [loaded, setLoaded] = useState(false);
@@ -9,7 +22,7 @@ const ImageComponent = ({ src, alt }) => {
 
   return (
     <div>
-      {!loaded && <p>Loading...</p>}
+      {!loaded && <MyImageLoader />}
       <img
         onLoad={handleImageLoaded}
         src={src}
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ContentLoader from "react-content-loader";
 
 function About() {
   return (
     <div className="about">
       <h1>Hello</h1>
       <p>I am this and that</p>
+      <ContentLoader />
     </div>
   );
 }
